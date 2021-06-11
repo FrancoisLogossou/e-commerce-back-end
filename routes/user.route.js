ab218f7cf9ff939d33a9eb2c6f2a3ff8e5230be7
CHATAUX <contact@jc-crea-design.com>
2021-06-11 16:48:00 +0200

    Jerome: validation creation

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const personneController = require('../controllers/personne.controller');
 
 router.get('/', personneController.getAll);
-router.get('/:id', personneController.getOneById);
+router.get('/:emailPersonne', personneController.getOneByEmail);
 router.get('/:idPersonne/adresses', personneController.getAdressesByIdPersonne);
 router.get('/:idPersonne/adresses/:idAdresse', personneController.getAdresseByIdPersonne);
 router.post('/', personneController.add);
