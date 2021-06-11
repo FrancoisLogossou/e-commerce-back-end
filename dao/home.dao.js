8e1d1b22cd72c6bb260470daa20b2de0b70b1365
Francois <francoislogossou@orange.fr>
2021-06-11 17:10:26 +0200

    francois: home modif, recupere article quand quantite differente de 0

--- dao/home.dao.js
+++ dao/home.dao.js
@@ -2,7 +2,7 @@ const connection = require('../database.js');
 
 exports.getAll = () => {
     return new Promise((resolve, reject) => {
-        const req = connection.query("SELECT * FROM article", (err, result) => {
+        const req = connection.query("SELECT * FROM article WHERE qteStock <> 0", (err, result) => {
             console.log(req.sql)
             err ? reject(err) : resolve(result);
         });
