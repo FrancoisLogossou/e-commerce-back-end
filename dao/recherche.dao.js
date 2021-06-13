5485da0227198a890fdb7ebc6332f4d22545ced4
Francois <francoislogossou@orange.fr>
2021-06-13 12:33:18 +0200

    francois: modif requête dans  recherche-dao

--- dao/recherche.dao.js
+++ dao/recherche.dao.js
@@ -2,13 +2,30 @@ const connection = require('../database.js');
 
 exports.getArticlesBySearch = (param1) => {
     return new Promise((resolve, reject) => {
-        const req = connection.query("SELECT * FROM article JOIN livre on article.refArticle = livre.refArticle LEFT JOIN auteurLivre on livre.numISBN = auteurLivre.numISBN LEFT JOIN auteur on auteurLivre.idAuteur = auteur.idAuteur WHERE article.titreArticle like ? OR auteur.nomAuteur like ? ", ["%"+param1+"%","%"+param1+"%"],  (err, result) => {
+        const req = connection.query("SELECT *,auteur.nomAuteur FROM article JOIN livre on article.refArticle = livre.refArticle LEFT JOIN auteurLivre on livre.numISBN = auteurLivre.numISBN LEFT JOIN auteur on auteurLivre.idAuteur = auteur.idAuteur WHERE article.titreArticle like ? OR auteur.nomAuteur like ? ", ["%"+param1+"%","%"+param1+"%"],  (err, result) => {
             console.log(req.sql)
             err ? reject(err) : resolve(result);
         });
     });
 };
+/*
 
+SELECT article.refArticle, imageArticle, titreArticle, puht, resumeArticle, livre.nomEditeur,nomAuteur FROM article LEFT JOIN livre ON article.refArticle = livre.refArticle LEFT JOIN auteurLivre ON livre.numISBN = auteurLivre.numISBN LEFT JOIN auteur ON auteurLivre.idAuteurLivre = auteur.idAuteur WHERE article.titreArticle like ? OR auteur.nomAuteur like ?
+"SELECT * FROM article JOIN livre on article.refArticle = livre.refArticle LEFT JOIN auteurLivre on livre.numISBN = auteurLivre.numISBN LEFT JOIN auteur on auteurLivre.idAuteur = auteur.idAuteur WHERE article.titreArticle like ? OR auteur.nomAuteur like ? "
+SELECT 
+    article.refArticle, 
+    imageArticle, 
+    titreArticle, 
+    puht,  
+    resumeArticle, 
+    livre.nomEditeur
+    nomAuteur 
+FROM article 
+    LEFT JOIN livre ON article.refArticle = livre.refArticle  
+    LEFT JOIN auteurLivre ON livre.numISBN = auteurLivre.numISBN 
+    LEFT JOIN auteur ON auteurLivre.idAuteurLivre = auteur.idAuteur 
+    WHERE article.titreArticle like '%ha%' OR auteur.nomAuteur like '%ha%'
+*/
 /*
 INSERT INTO livre (   
     numISBN,
