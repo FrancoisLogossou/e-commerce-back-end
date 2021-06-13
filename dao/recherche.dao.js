0ef1fb4722cf73469d056fa25c48b551090661d9
CHATAUX <contact@jc-crea-design.com>
2021-06-13 17:28:26 +0200

    Jerome: validation recherche dao

--- dao/recherche.dao.js
+++ dao/recherche.dao.js
@@ -2,7 +2,7 @@ const connection = require('../database.js');
 
 exports.getArticlesBySearch = (param1) => {
     return new Promise((resolve, reject) => {
-        const req = connection.query("SELECT * FROM article JOIN livre on article.refArticle = livre.refArticle LEFT JOIN auteurLivre on livre.numISBN = auteurLivre.numISBN LEFT JOIN auteur on auteurLivre.idAuteur = auteur.idAuteur WHERE article.titreArticle like ? OR auteur.nomAuteur like ? ", ["%"+param1+"%","%"+param1+"%"],  (err, result) => {
+        const req = connection.query("SELECT * FROM article JOIN livre ON article.refArticle = livre.refArticle LEFT JOIN auteurLivre ON livre.numISBN = auteurLivre.numISBN LEFT JOIN auteur ON auteurLivre.idAuteur = auteur.idAuteur WHERE article.titreArticle like ? OR auteur.nomAuteur like ? ", ["%"+param1+"%","%"+param1+"%"],  (err, result) => {
             console.log(req.sql)
             err ? reject(err) : resolve(result);
         });
